6b5108d89dda819d39709d3cbfbad056b7bf707b
Ana Ea <aabad0464@gmail.com>
2021-09-01 04:36:21 -0600

    creating db.js

--- public/db.js
+++ public/db.js
@@ -1,9 +1,54 @@
 let db;
-
 // Connection to new Database
 const request = indexedDB.open('budget_tracker', 1);
 
+request.onupgradeneeded = function (event) {
+    const db = event.target.result;
+    db.createObjectStore("pending", { autoIncrement: true });
+  };
+//Established a upon success
+  request.onsuccess = function (event) {
+    db = event.target.result;
+    if (navigator.onLine) {
+      checkDatabase();
+    }
+  };
 
-
-// listen for app coming back online
-window.addEventListener('online', uploadTransaction);
+//Upon error
+  request.onerror = function (event) {
+    console.log("There was an error " + event.target.errorCode);
+  };
+  
+  function saveRecord(record) {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    store.add(record);
+  }
+  
+  function checkDatabase() {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    const getAll = store.getAll();
+  
+    getAll.onsuccess = function () {
+      if (getAll.result.length > 0) {
+        fetch("/api/transaction/bulk", {
+          method: "POST",
+          body: JSON.stringify(getAll.result),
+          headers: {
+            Accept: "application/json, text/plain, */*",
+            "Content-Type": "application/json",
+          },
+        })
+          .then((response) => response.json())
+          .then(() => {
+            const transaction = db.transaction(["pending"], "readwrite");
+            const store = transaction.objectStore("pending");
+  
+            store.clear();
+          });
+      }
+    };
+  }
+  
+  window.addEventListener("online", checkDatabase);
